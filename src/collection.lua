0274adddf51336e4afc91b57a212d0717254c973
Raimon Grau <raimonster@gmail.com>
2013-07-20 22:45:13 +0200

    collection. creation/deletion of collection

--- src/collection.lua
+++ src/collection.lua
@@ -1,1 +1,72 @@
 -- collection.lua
+local DEFAULT_OPTIONS = {
+      max_documents = 1000,
+      raw = false,
+      only_ids = false,
+      reversed = false,
+      excluded_fields_to_index = {}
+    }
+
+local collection = {}
+
+local function idx_set_key(namespace, name, id)
+   local t= {namespace, name, "sidx", id}
+   return table.concat(t, "/")
+end
+
+local function doc_sset_key(namespace, name)
+   return(table.concat({namespace, name, "ssdocs"}, "/")
+end
+
+local function doc_key(namespace, name, id)
+   return(table.concat({namespace, name, "docs", id}, "/"))
+end
+
+function collection.delete_all(coll_name, doc)
+   if not coll_name then
+      error("coll_name has to be there")
+   end
+   if not doc then
+      error("doc error")
+   end
+
+   local ids = collection.find(doc, {only_ids=true, max_documents = -1})
+   for _,id in ipairs(ids) do
+      delete_element(coll_name, id)
+   end
+end
+
+function collection.delete_element(coll_name ,id)
+   local redis = redis()
+   local indexes = redis:smembers(idx_set_key("jor", coll_name, id))
+   for _,index in ipairs(indexes) do
+      remove_index(index, id)
+   end
+   redis:del(idx_set_key("jor", coll_name, id))
+   redis:zrem(doc_sset_key("jor", coll_name), id)
+   redis:del(doc_key("jor", coll_name, id))
+end
+
+function remove_index(index, id)
+   local key = string.sub(index, 1, -6)
+   local action = string.sub(index, -5)
+   if  action == "_srem" then
+      redis:srem(key, id)
+   elseif action == "_zrem"  then
+      redis:zrem(key, id)
+   else error("unknown index") end
+end
+
+function merge_with_default_options(opts)
+   local res
+   for k, v in pairs(opts) do
+      res[k] = v
+   end
+   for k, v in pairs(DEFAULT_OPTIONS) do
+      res[k] = res[k] or v
+   end
+   return res
+end
+
+
+return collection
--- src/storage.lua
+++ src/storage.lua
@@ -1,4 +1,10 @@
 -- storage.lua
+-- > redis = require 'redis'
+-- > client= redis.connect('127.0.0.1', 6379)
+-- > client:ping()
+local collection = require 'collection'
+local redis
+
 
 local namespace = "jor"
 local selectors = {
@@ -13,22 +19,53 @@ for _,v in pairs(selectors) do
    end
 end
 
-local function redis(config)
-   return "redis"
+local function redis()
+   local r= require 'redis'
+   redis = redis or r.connect('127.0.0.1', 6379)
+   return redis
 end
-
-local redis = redis()
 
 function create_collection(name, options)
    options = options or {}
-
-   if not collection_exists(name) then
-      internal_create(name, options)
-   else
+   local auto_inc = not not options.auto_increment
+   local new
+   new = internal_create_collection(name, auto_inc)
+   if not new then
       error("CollectionNotValid")
    end
 end
 
-function internal_create(name, options)
-   redis.sadd(namespace .. "/collections", name)
+function internal_create_collection(name, auto_inc)
+   local redis = redis()
+   local is_new = redis:sadd(namespace .. "/collections", name)
+   if is_new == 0 then
+     return nil, "error"
+   end
+   redis:set(namespace .. "/collection/" .. name .. "/auto-increment", auto_inc )
+   return name
+end
+
+function destroy_all()
+   for _, name in pairs(collections()) do
+      destroy_collection(name)
+   end
+end
+
+function destroy_collection(name)
+   local redis = redis()
+
+   collection.delete_all(namespace , name)
+   -- pipelined
+   redis:srem(namespace.. "/collections", name)
+   redis:del(namespace.. "/collection/".. name .."/auto-increment")
+   -- pipelined-end
+end
+
+function find_collection(name)
+   local redis = redis()
+   local redis_auto_incr = redis.get(namespace.."/collection/".. name .."/auto-increment")
+   if redis_auto_incr == "true" then
+   elseif redis_auto_incr == "false" then
+   else error("Collection " .. name .." doesn't exist")
+   end
 end
